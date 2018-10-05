2e08b6662d90fb6eca3a19179747498adc490016
Adam Kocić <adam.kocic@hotmail.com>
2018-10-05 17:19:14 +0200

    coupon card style 1

--- src/js/couppy.js
+++ src/js/couppy.js
@@ -32,11 +32,47 @@
     const Couppy = {}; // Object for public APIs
     const supports = !!document.querySelector && !!root.addEventListener; // Feature test
     let settings, eventTimeout;
+    const pluginName = 'Couppy';
+    const pluginNameLower = pluginName.toLowerCase();
+    const pluginClassPrefix = `js-${pluginNameLower}`;
 
     // Default settings
     const defaults = {
         someVar: 123,
         initClass: 'js-Couppy',
+
+        state: {
+            active: true,
+            open: false,
+        },
+        appearance: {
+            style: 1, // card render style
+            main: {
+                classList: ['couppy-1']
+            },
+            overlay: {
+                classList: []
+            },
+            popupContainer: {
+                classList: []
+            },
+            popup: {
+                classList: []
+            },
+            card: {
+                classList: []
+            }
+        },
+        refs: {
+            overlay: null,
+            popupContainer: null,
+            popup: null,
+            card: null,
+            btn: {
+                close: null
+            }
+        },
+
         callbackOnInit: function() {
         },
         callbackBefore: function () {
@@ -177,6 +213,38 @@
     };
 
     /**
+     * Return class prefix
+     * @private
+     * @param  {String} className
+     */
+    const classPrefix = function (className) {
+        return `${pluginClassPrefix}__${className}`;
+    };
+
+    /**
+     * Format an array of classes to a string
+     * @private
+     * @param  {Array} classes
+     */
+    const formatClasses = function (classes) {
+        if(classes instanceof Array) {
+            let classes_string = '';
+            classes.forEach(function(item, i) {
+                classes_string += item;
+                classes_string += i < classes.length - 1 ? ' ' : '';
+            });
+
+            return classes_string;
+        } else if(classes instanceof String) {
+            return classes;
+        }
+
+        return '';
+    };
+
+    /* =============== PUBLIC FUNCTIONS =============== */
+
+    /**
      * Initialize Plugin
      * @public
      * @param {Object} options User settings
@@ -195,14 +263,171 @@
         document.documentElement.classList.add(settings.initClass);
 
         // @todo Do something...
+        Couppy.renderHtml(settings.appearance.style);
+        settings.state.open ? Couppy.open() : Couppy.close();
 
         // Listen for events
-        document.addEventListener('click', eventHandler, false);
+        settings.refs.overlay.addEventListener('click', function(event) {
+            Couppy.close();
+        }, false);
+        settings.refs.popup.addEventListener('click', function(event) {
+            event.stopPropagation();
+        }, false);
 
         // On Init callback
         if (typeof settings.callbackOnInit === 'function') {
             settings.callbackOnInit.call(this);
         }
+    };
+
+    /**
+     * Open popup
+     * @public
+     */
+    Couppy.open = function () {
+        settings.refs.overlay.classList.remove('hidden');
+
+        // On Open callback
+        if (typeof settings.callbackOnOpen === 'function') {
+            settings.callbackOnOpen.call(this);
+        }
+    };
+
+    /**
+     * Close popup
+     * @public
+     */
+    Couppy.close = function () {
+        settings.refs.overlay.classList.add('hidden');
+
+        // On Close callback
+        if (typeof settings.callbackOnClose === 'function') {
+            settings.callbackOnClose.call(this);
+        }
+    };
+
+    /* =============== PRIVATE FUNCTIONS / HELPERS =============== */
+
+    /**
+     * Render card html
+     * @private
+     * @param {String} style ID of card style to use
+     */
+    Couppy.renderHtml = function (style) {
+        switch(style) {
+            case 1:
+                Couppy.renderHtml_Style1();
+                break;
+            default:
+                break;
+        }
+    };
+
+    /**
+     * Render card style 1
+     * @private
+     */
+    Couppy.renderHtml_Style1 = function () {
+        const body = document.getElementsByTagName('body')[0];
+        const main = document.createElement('div');
+        main.classList.add(...[pluginClassPrefix].concat(settings.appearance.main.classList)); // add multiple classes using spread syntax
+        settings.refs.main = body.appendChild(main);
+
+        /* ============== */
+
+        /**
+         * Render HTML overlay
+         * @private
+         */
+        const templateHtml_Overlay = function() {
+            const htmlTemplate = `
+            `;
+            return htmlTemplate;
+        };
+
+        /**
+         * Render HTML popup container
+         * @private
+         */
+        const templateHtml_PopupContainer = function() {
+            const htmlTemplate = `
+            `;
+            return htmlTemplate;
+        };
+
+        /**
+         * Render HTML popup
+         * @private
+         */
+        const templateHtml_Popup = function() {
+            const htmlTemplate = `
+            `;
+            return htmlTemplate;
+        };
+
+        /**
+         * Render HTML card
+         * @private
+         */
+        const templateHtml_Card = function() {
+            const htmlTemplate = `
+            <div class="${classPrefix('c-body')}">
+                <h1 class="${classPrefix('tx-title')}">Halloween sale</h1>
+                <p class="${formatClasses([classPrefix('tx-title'), classPrefix('sp-highlight')])}"><span class="${formatClasses([classPrefix('sp-super'), classPrefix('sp-highlight')])}">5% off</span></p>
+                <p class="${classPrefix('tx-code')}">COUPONCODE</p>
+                <p class="${classPrefix('tx-subtext')}">use the code above during the checkout</p>
+            </div>
+            
+            <div class="${classPrefix('c-footer')}">
+                <p><a href="#" class="${classPrefix('tx-link')}">See the collection <i class="fas fa-arrow-right"></i></a></p>
+            </div>
+           `;
+            return htmlTemplate;
+        };
+
+        /**
+         * Render HTML close button
+         * @private
+         */
+        const templateHtml_BtnClose = function() {
+            const htmlTemplate = `
+            <i class="fas fa-times"></i>
+            `;
+            return htmlTemplate;
+        };
+
+        /* ============== */
+
+        // Render overlay
+        const couppyOverlay = document.createElement('div');
+        couppyOverlay.classList.add(...[classPrefix('overlay')].concat(settings.appearance.overlay.classList)); // add multiple classes using spread syntax
+        couppyOverlay.innerHTML = templateHtml_Overlay();
+        settings.refs.overlay = main.appendChild(couppyOverlay);
+
+        // Render popup container
+        const couppyPopupContainer = document.createElement('div');
+        couppyPopupContainer.classList.add(...[classPrefix('popup-container')].concat(settings.appearance.popupContainer.classList)); // add multiple classes using spread syntax
+        couppyPopupContainer.innerHTML = templateHtml_PopupContainer();
+        settings.refs.popupContainer = couppyOverlay.appendChild(couppyPopupContainer);
+
+        // Render popup
+        const couppyPopup = document.createElement('div');
+        couppyPopup.classList.add(...[classPrefix('popup')].concat(settings.appearance.popup.classList)); // add multiple classes using spread syntax
+        couppyPopup.innerHTML = templateHtml_Popup();
+        settings.refs.popup = couppyPopupContainer.appendChild(couppyPopup);
+
+        // Render container
+        const couppyCard = document.createElement('div');
+        couppyCard.classList.add(...[classPrefix('container')].concat(settings.appearance.card.classList)); // add multiple classes using spread syntax
+        couppyCard.innerHTML = templateHtml_Card();
+        settings.refs.card = couppyPopup.appendChild(couppyCard);
+
+        // Render Close Button
+        const couppyBtnClose = document.createElement('span');
+        couppyBtnClose.classList.add(classPrefix('btn-close'));
+        couppyBtnClose.setAttribute("role", "button");
+        couppyBtnClose.innerHTML = templateHtml_BtnClose();
+        settings.refs.btn.close = couppyCard.appendChild(couppyBtnClose);
     };
 
 
