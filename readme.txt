9386597a8161c92974ddbeb05ebaa6dcfdcc8751
Adam Kocić <adam.kocic@hotmail.com>
2018-12-13 18:09:19 +0100

    update - cleanup

--- readme.txt
+++ readme.txt
@@ -1,2 +1,6 @@
+required:
+npm install browserify
+npm install uglifyjs
+
 Compile new code to bundle.js:
 browserify src/js/app.js | uglifyjs > src/js/bundle.js
--- src/js/couppy.js
+++ src/js/couppy.js
@@ -1419,6 +1419,8 @@
                 `;
             }
 
+            /* --- */
+
             const htmlTemplate = `
             <div class="${classPrefix('c-body')}">
                 ${titleHTML}
